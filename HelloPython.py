d3d4e7979fcb071fbb7cf5e3496326af4f3a992a
taptiPC <taptisoni@gmail.com>
2019-09-17 22:09:42 +0530

    changed code to take user input

--- HelloPython.py
+++ HelloPython.py
@@ -6,3 +6,10 @@ Created on Tue Sep 17 21:19:20 2019
 """
 
 print("\nHello Python and Anaconda")
+checkPA = input("ENter P for Python or A for anaconda\n")
+if checkPA=='p':
+    print("Hello Python")
+elif checkPA=='a': 
+    print("Hello Anaconda")
+else:
+    print("Wrong Option selected - exit")
